8e0da04e7964fd73b386471308613684b0633f87
Joerg Jung <joerg-jung@web.de>
2016-03-22 16:52:45 +0100

    add pull to refresh feature

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,4 +2,13 @@ var app = angular.module('caffeinehit.controllers', []);
 
 app.controller("YelpController", function($scope, YelpService) {
 	$scope.yelp = YelpService;
+    
+    $scope.doRefresh = function() {
+        if (!$scope.yelp.isLoading) {
+            $scope.yelp.refresh().then(function() {
+                // when refreshed then broadcast this event hide spinner icon
+                $scope.$broadcast('scroll.refreshComplete');
+            });
+        }        
+    };
 });
