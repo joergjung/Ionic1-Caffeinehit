d4597c810590a8f70ed95a5e26d11c51f037f61a
Joerg Jung <joerg-jung@web.de>
2016-03-22 21:49:07 +0100

    add cordova geolocation

--- www/js/services.js
+++ www/js/services.js
@@ -1,6 +1,6 @@
 var app = angular.module('caffeinehit.services', []);
 
-app.service("YelpService", function($q, $http) {
+app.service("YelpService", function($q, $http, $cordovaGeolocation, $ionicPopup) {
 	var self = {
 		'page': 1,
 		'isLoading': false,
@@ -23,29 +23,47 @@ app.service("YelpService", function($q, $http) {
 		'load': function() {
 			self.isLoading = true;
 			var deferred = $q.defer();
-			var params = {
-				page: self.page,
-				lat: self.lat,
-				lon: self.lon
-			};
-            // use the codecraftpro service wrapper 
-			$http.get('https://codecraftpro.com/api/samples/v1/coffee/', {params: params})
-				.success(function (data) {
-					self.isLoading = false;
-					console.log(data);
-					if (data.businesses.length === 0) {
-						self.hasMore = false;
-					} else {
-						angular.forEach(data.businesses, function(business) {
-							self.results.push(business);
-						});
-					}
-					deferred.resolve();
-				})
-				.error(function(data, status, headers, config) {
-					self.isLoading = false;
-					deferred.reject(data);
-				});
+            
+            // first check if device has fully loaded and if the plugins are available 
+            ionic.Platform.ready(function() {
+                $cordovaGeolocation
+                    .getCurrentPosition({ timeout: 10000, enableHighAccuracy: false })
+                    .then(function(position) {
+                        self.lat = position.coords.latitude;
+                        self.lon = position.coords.longitude;
+                        
+                        var params = {
+                            page: self.page,
+                            lat: self.lat,
+                            lon: self.lon
+                        };
+                        // use the codecraftpro service wrapper 
+                        $http.get('https://codecraftpro.com/api/samples/v1/coffee/', {params: params})
+                            .success(function (data) {
+                                self.isLoading = false;
+                                console.log(data);
+                                if (data.businesses.length === 0) {
+                                    self.hasMore = false;
+                                } else {
+                                    angular.forEach(data.businesses, function(business) {
+                                        self.results.push(business);
+                                    });
+                                }
+                                deferred.resolve();
+                            })
+                            .error(function(data, status, headers, config) {
+                                self.isLoading = false;
+                                deferred.reject(data);
+                            });
+                    }, function(err) {
+                        console.error("Error getting position");
+                        console.error(err);   
+                        $ionicPopup.alert({
+                            'title':'Geolocation disabled!',
+                            'template':'Seems like you switched off geolocation for caffeinehit, please switch it on by going to your application settings. Thanks!'
+                        });                      
+                    });
+            });
 			return deferred.promise;
 		}
 	};
